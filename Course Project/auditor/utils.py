03d8206b07cc36e4e33f4907ab19991d7e6e7581
michaelfdickey <michaelfdickey@github.com>
2022-04-02 13:08:19 -0700

    implemented str_to_time and daycycle in utils passing step11 tests

--- Course Project/auditor/utils.py
+++ Course Project/auditor/utils.py
@@ -15,6 +15,9 @@ Date: Apr 02 2022
 """
 import csv
 import json
+import datetime
+import pytz
+from dateutil.parser import parse
 
 
 def read_csv(filename):
@@ -31,7 +34,7 @@ def read_csv(filename):
     is a valid CSV file
     """
     
-    print(" running read_csv")
+    #print(" running read_csv")
 
     # verify input
     #print(" filename is: ", filename)
@@ -75,7 +78,7 @@ def write_csv(data,filename):
     .csv or .CSV.  The file may or may not exist.
     """
     # print tracing
-    print(" running write_csv")
+    #print(" running write_csv")
 
     # verify input
     #print(" data is: ", data)
@@ -111,7 +114,7 @@ def read_json(filename):
     """
     # verify input
     #print(" filename is: ", filename)
-    print(" running read_json")
+    #print(" running read_json")
 
     # open file
     file_opened = open(filename)
@@ -157,7 +160,66 @@ def str_to_time(timestamp,tz=None):
     """
     # HINT: Use the code from the previous exercise and update the timezone
     # Use localize if timezone is a string; otherwise replace the timezone if not None
-    pass
+
+   # verify inputs
+    #print(" timestamp is: ", timestamp, "tz is: ", tz)
+
+    # try to convert with parse function
+    try:
+        dt_timestamp = parse(timestamp)
+        #print(" dt_timestamp is: ", dt_timestamp)
+    except:
+        ## if invalid return none
+        return None
+
+    # check if it has a timezone (even if empty)
+    if dt_timestamp.tzinfo != None:
+        #print("  dt_timestamp has a tz and is a valid datetime object, returning dt_timestamp")
+        return dt_timestamp
+
+    if dt_timestamp.tzinfo == None:
+        #print("  dt_timestamp tzinfo is None, adding tz")
+
+        ## if no timezone and tz = none, return datetime
+        if tz == None:
+            #print("   tz argument was blank: ", tz, "returning datetime as is")
+            #print("   dt_timestamp is: ", dt_timestamp)
+            return dt_timestamp
+
+        ## if no timezone and tz!=None give datetime tz
+        if tz != None:
+            if isinstance(tz, str) == False:    #ignore if tz isn't empty but also isn't a string
+                #print("   no timezone in timestamp, tz not empty, adding tz to datetime object")
+                #print("    dt_timestamp is: ", dt_timestamp)
+                #print("    tz is: ", tz)
+                #print("    type(tz) is: ", type(tz))
+
+                dt_timestamp_new = dt_timestamp.replace(tzinfo=tz)
+
+                return dt_timestamp_new
+
+        ## if tz is string convert and localize it
+        if isinstance(tz, str) == True:
+            #print("    tz is: ", repr(tz))
+            #print("    tz is a string, converting with pytz")
+            tza = pytz.timezone(tz)
+            #print("    tz is now: ", repr(tza))
+            #print("    type(tza) is: ", type(tza))
+
+            #print("     str(dt_timestamp) is: ", repr(str(dt_timestamp)))
+            dt_timestamp_new = dt_timestamp.replace(tzinfo=tza)
+            #print("     str(dt_timestamp_new) is: ", repr(str(dt_timestamp_new)))
+
+            dt_timestamp_localized = tza.localize(dt_timestamp)
+
+            #tzb = tza.localize()     
+            #dt_timestamp_localized = tz.localize(dt_timestamp_new)
+
+            #print("      dt_timestamp_localized is: ", dt_timestamp_localized)
+
+            return dt_timestamp_localized
+        
+        
 
 
 def daytime(time,daycycle):
@@ -200,7 +262,132 @@ def daytime(time,daycycle):
     """
     # HINT: Use the code from the previous exercise to get sunset AND sunrise
     # Add a timezone to time if one is missing (the one from the daycycle)
-    pass
+
+    # verify inputs
+    #print(" time is: ", time)
+
+    # convert time input to a valid datetime object
+    #print(" type(time) is: ", type(time))
+
+    # get year
+    year = time.year
+    #print(" year is: ", year)   
+
+    # get year dictionary
+    year_dictionary = daycycle[str(year)]
+    ##print(" year_dictionary is: ", year_dictionary)
+
+    # get mm-dd
+    ## get month
+    month = str(time.month)
+    #print(" month is: ", month)
+    if len(month) < 2:
+        month = "0" + month
+        #print("  month is now: ", month)
+    
+    ## get day
+    day = str(time.day)
+    #print(" day is: ", day)
+    if len(day) < 2:
+        day = "0" + day 
+        #print("  day is now: ", day)
+
+    mmdd = month + "-" + day
+    #print(" mmdd is: ", mmdd)
+    
+    # get mmdd dictionary
+    month_dictionary = year_dictionary[mmdd]
+    #print(" month_dictionary is: ", month_dictionary)
+
+    
+    # get sunrise info
+    sunrise = month_dictionary['sunrise']
+    #print(" sunrise is: ", sunrise)
+    
+    ## get hours and minutes for sunrise
+    sunrise_hours = sunrise[0:2]
+    if sunrise_hours[0] == '0':
+        sunrise_hours = (sunrise_hours[1:2])
+        #print("   sunrise_hours is now: ", sunrise_hours)
+    
+    sunrise_minutes = sunrise[3:5]
+    if sunrise_minutes[0] == '0':
+        sunrise_minutes = (sunrise_minutes[1:])
+        #print("   sunrise_minutes is now: ", sunrise_minutes)
+
+    #print("   sunrise_hours is: ", sunrise_hours, "sunrise_minutes is: ", sunrise_minutes)
+    
+
+    # get sunset info 
+    sunset = month_dictionary['sunset']
+    #print(" sunset is: ", sunset)
+
+    ## get hours and minutes for sunset
+    sunset_hours = sunset[0:2]
+    if sunset_hours[0] == '0':
+        sunset_hours = (sunset_hours[1:2])
+        #print("   sunset_hours is now: ", sunset_hours)
+
+    sunset_minutes = sunset[3:5]
+    if sunset_minutes[0] == '0':
+        sunset_minutes = (sunset_minutes[1:])
+        #print("   sunset_minutes is now: ", sunset_minutes)
+
+    #print("   sunset_hours is: ", sunset_hours, "sunset_minutes is: ", sunset_minutes)
+
+
+
+
+
+    # create sunrise, sunset, and now objects:
+    #print("   time.tzinfo is: ", time.tzinfo)
+    is_aware = time.tzinfo != None              # does time have a tz?
+    #print("   is_aware is: ", is_aware)
+    
+    if is_aware == True:
+        #print("    time has a tz")
+        tz = pytz.timezone('America/New_York')                                                                          #sunrise/set tz from daycycle.json        
+        
+        # create sunrise
+        sunrise_naive = datetime.datetime(int(year),int(month),int(day),int(sunrise_hours),int(sunrise_minutes))        #create datetime object without tz
+        #print("      sunrise is: ", sunrise)
+        sunrise = tz.localize(sunrise_naive)                                                                            #localize it with tz.localize method
+        #print("      sunrise is: ", sunrise)
+
+        # create sunset
+        sunset_naive = datetime.datetime(int(year),int(month),int(day),int(sunset_hours),int(sunset_minutes))
+        #print("      sunset is: ", sunset)
+        sunset = tz.localize(sunset_naive)
+        #print("      sunset is: ", sunset)
+
+        # create now
+        now = time
+
+
+    if is_aware == False:
+        #print("    time has no tz")
+        pass
+
+
+    # check values
+    #print("   sunrise is: ", sunrise)
+    #print("   now is:     ", now)
+    #print("   sunset is:  ", sunset)
+
+    # evaluate
+    if now <= sunrise:
+        #print(" now is before sunrise, returning false")
+        return False
+
+    if now > sunrise:
+        
+        if now < sunset:
+            #print(" sunrise < now < sunset is true ")
+            return True
+
+        if now >= sunset:
+            #print(" now is after sunset, returning false")
+            return False
 
 
 def get_for_id(id,table):
