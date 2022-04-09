e82a93da0e3cbf6c006be8da4f4f3c8019587f12
michaelfdickey <michaelfdickey@github.com>
2022-04-09 00:21:52 -0700

    app discover violations passed all tsts locally

--- Course Project/auditor/app.py
+++ Course Project/auditor/app.py
@@ -86,12 +86,12 @@ def discover_violations(directory,output):
 
     # prepare output filename and write csv file
     if output != None:
-        filename = output + '.csv'
+        #filename = output + '.csv'
         #print(" filename will be: ", filename)
 
         # write csv file
-        output_csv_path = os.path.join(directory,filename)
-        output_csv_file = open(output_csv_path,'w',newline='')
+        #output_csv_path = os.path.join(directory,filename)
+        output_csv_file = open(output,'w',newline='')
         output_csv_wrapped = csv.writer(output_csv_file)
         for row in discovered_violations_table:
             output_csv_wrapped.writerow(row)
